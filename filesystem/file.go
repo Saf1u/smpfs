0bc1689d6599d38783c413cb0895f48fb54ae9d9
Saf1u <a_safwa@live.concordia.ca>
2023-06-13 15:15:53 -0400

    file strucutre agumentation

--- filesystem/file.go
+++ filesystem/file.go
@@ -16,6 +16,7 @@ type file struct {
 type File interface {
 	setCreationTs(time.Time)
 	updateAccessTs(time.Time)
+	getSize() int
 	getManifest() *disk.BlockRecord
 	setManifest(*disk.BlockRecord)
 }
@@ -31,11 +32,11 @@ func (fl *file) name() string {
 	return fl.fileName
 }
 func (fl *file) updateAccessTs(time time.Time) {
-
+	fl.lastModified = time
 }
 
 func (fl *file) setCreationTs(time time.Time) {
-
+	fl.createdAt = time
 }
 
 func (fl *file) getManifest() *disk.BlockRecord {
@@ -43,5 +44,9 @@ func (fl *file) getManifest() *disk.BlockRecord {
 }
 
 func (fl *file) setManifest(manifest *disk.BlockRecord) {
-	fl.info=manifest
+	fl.info = manifest
+}
+
+func (fl *file) getSize() int {
+	return 0
 }
